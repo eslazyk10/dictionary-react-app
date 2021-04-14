f2b09047009c72437241eb2c8180ab912bd03997
eslazyk10 <eslazy32@gmail.com>
2021-04-14 16:02:52 -0500

    altering image size

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ export default function Header(){
     return(
         <div className="Header">
             <h1>
-        <img src="https://www.filipinostatic.com/images/section_icons/dictionary2.png" alt="logo" width="100"/> 
+        <img src="https://www.filipinostatic.com/images/section_icons/dictionary2.png" alt="logo" width="75"/> 
         {" "}<span className="Title">Dictionary</span>{" "}
-        <img src="https://www.filipinostatic.com/images/section_icons/dictionary2.png" alt="logo" width="100"/>
+        <img src="https://www.filipinostatic.com/images/section_icons/dictionary2.png" alt="logo" width="75"/>
         </h1>
         <h5>
           What would you like to define...
